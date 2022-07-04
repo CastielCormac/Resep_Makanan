3cd1cea5f2e44da9b181164e074caee223a03730
Rizky Wahyu Kristiawan <88009892+CastielCormac@users.noreply.github.com>
2022-07-05 01:56:04 +0700

    Add files via upload

---
+++ lib/static/static_values.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kApiURLDomain = 'api.spoonacular.com';
+const kApiEndPointRecipe = 'recipes/complexSearch';
+const kApiKey = '755304f74bc245aaa3db1baf7800153c';
+
+const kPrimaryColor = Colors.redAccent;
